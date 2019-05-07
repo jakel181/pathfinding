21f51282837fcd37398e7672be117c680eeaf4a9
jakel181 <17256688+jakel181@users.noreply.github.com>
2019-05-07 08:01:20 -0400

    Update pathfinding.js

--- pathfinding.js
+++ pathfinding.js
@@ -25,3 +25,65 @@ var currentPath = [];
 
 // ensure that concole.log doesn't cause errors
 if (typeof console == "undefined") var console = { log: function() {} };
+
+// the html page is ready
+function onload() 
+{
+  console.log('Page loaded.');
+  canvas = document.getElementById('gameCanvas');
+  canvas.width = worldWidth * tileWidth;
+  canvas.height = worldHeight * tileHeight;
+  canvas.addEventListener("click", canvasClick, false);
+  ctx = canvas.getContext("2d");
+  spritesheet = new Image();
+  spritesheet.src = 'spritesheet.png';
+  spritesheet.onload = loaded;
+}
+
+// the spritesheet is ready
+function loaded() 
+{
+  console.log('Spritesheet loaded.');
+  spritesheetLoaded = true;
+  createWorld();
+}
+
+// fill the world with walls
+function createWorld()
+{
+  console.log('Creating world...');
+  
+  // create emptiness
+  for (var x=0; x < worldWidth; x++)
+  {
+    world[x] = [];
+    
+    for (var y=0; y < worldHeight; y++)
+    {
+      world[x][y] = 0;
+    }
+  }
+  
+  // scatter some walls
+  for (var x=0; x < worldWidth; x++)
+  {
+    for (var y=0; y < worldHeight; y++)
+    {
+      if (Math.random() > 0.75)
+        world[x][y] = 1;
+    }
+  }
+  
+  // calculate initial possible path
+  // note: unlikely but possible to never find one...
+  currentPath = [];
+  while (currentPath.length == 0) 
+  {
+    pathStart = [Math.floor(Math.random()*worldWidth),Math.floor(Math.random()*worldHeight)];
+    pathEnd = [Math.floor(Math.random()*worldWidth),Math.floor(Math.random()*worldHeight)];
+    if (world[pathStart[0]][pathStart[1]] == 0)
+      currentPath = findPath(world,pathStart,pathEnd,'Manhattan');
+  }
+  redraw();
+  
+}
